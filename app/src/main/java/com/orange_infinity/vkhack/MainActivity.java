fdb7dfda25c7e1bc9464ccff5dfd832d672a5d83
Jenya Bykov <mothersasg@yandex.ru>
2019-09-28 05:22:33 +0300

    save

--- app/src/main/java/com/orange_infinity/vkhack/MainActivity.java
+++ app/src/main/java/com/orange_infinity/vkhack/MainActivity.java
@@ -1,10 +1,26 @@
 package com.orange_infinity.vkhack;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.orange_infinity.vkhack.ui.activities.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends BaseActivity {
+
+    private List<String> skills = new ArrayList<>();
+    private SkillHolder skillHolder;
+    private SkillAdapter skillAdapter;
+    private RecyclerView skillRecycler;
 
     public MainActivity() {
         super(0);
@@ -16,5 +32,69 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         setUpBottomNavigation();
+        skills.add("Hello");
+        skills.add("Kitty");
+        skills.add("Motherfucker");
+        skills.add("fu*ck");
+        skills.add("mamaamakriminak");
+        skills.add("255");
+
+        setUpRecycler();
+    }
+
+    private void setUpRecycler() {
+        skillAdapter = new SkillAdapter(skills);
+        skillRecycler = findViewById(R.id.recyclerSkills);
+        skillRecycler.setLayoutManager(new GridLayoutManager(this, 3));
+        skillRecycler.setAdapter(skillAdapter);
+    }
+
+    private void updateUi() {
+        skillAdapter.skills = skills;
+        skillAdapter.notifyDataSetChanged();
+    }
+
+    private class SkillHolder extends RecyclerView.ViewHolder {
+
+        private View skillView;
+
+        SkillHolder(View skillView) {
+            super(skillView);
+            this.skillView = skillView;
+        }
+
+        private void bindView(String skill) {
+            TextView tvSkill = skillView.findViewById(R.id.tvSkill);
+            tvSkill.setText(skill);
+        }
+    }
+
+    private class SkillAdapter extends RecyclerView.Adapter<SkillHolder>{
+
+        private List<String> skills;
+
+        SkillAdapter(List<String> skills) {
+            this.skills = skills;
+        }
+
+        @NonNull
+        @Override
+        public SkillHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+            View view = layoutInflater.inflate(R.layout.skill_list , parent, false);
+
+            return new SkillHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SkillHolder holder, int position) {
+            String skill = skills.get(position);
+            holder.bindView(skill);
+        }
+
+        @Override
+        public int getItemCount() {
+            return skills.size();
+        }
     }
 }
